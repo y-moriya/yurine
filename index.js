62b4cb7549e3ed3282285643b0a87c02cd497102
Yu MORIYA <euro21st@gmail.com>
2023-09-01 17:03:10 +0900

    Add .gitignore, config.example.json, index.js, and package.json

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, Intents } = require('discord.js');
+const { token, roleId } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES], partials: ['MESSAGE', 'USER'] });
+
+client.on('messageDelete', ctx => {
+  if (!ctx.member?.roles.cache.has(roleId)) {
+      return
+  }
+
+  const content = ctx.content
+
+  // こうしないとボットのメッセージを消すと一生タグが増える、やればわかる
+  if (ctx.author?.bot) {
+      ctx.channel.send(`${content}`)
+      return
+  }
+
+  ctx.channel.send(`${content}`)
+})
+
+client.login(token);
