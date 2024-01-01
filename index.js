1a9313c155b3acc6eaafec0decda8b1eb8e718b3
Yu MORIYA <euro21st@gmail.com>
2024-01-01 17:37:56 +0900

    Add events on messageUpdate

--- index.js
+++ index.js
@@ -5,18 +5,34 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 client.on('messageDelete', ctx => {
   if (!ctx.member?.roles.cache.has(roleId)) {
-      return
+    return
   }
 
   const content = ctx.content
 
   // こうしないとボットのメッセージを消すと一生タグが増える、やればわかる
   if (ctx.author?.bot) {
-      ctx.channel.send(`${content}`)
-      return
+    ctx.channel.send(`${content}`)
+    return
   }
 
   ctx.channel.send(`${content}`)
+})
+
+client.on('messageUpdate', (oldMessage, newMessage) => {
+  if (!oldMessage.member?.roles.cache.has(roleId)) {
+    return
+  }
+
+  const content = oldMessage.content
+
+  // こうしないとボットのメッセージを消すと一生タグが増える、やればわかる
+  if (oldMessage.author?.bot) {
+    oldMessage.channel.send(`${content}`)
+    return
+  }
+
+  oldMessage.channel.send(`${content}`)
 })
 
 client.login(token);
